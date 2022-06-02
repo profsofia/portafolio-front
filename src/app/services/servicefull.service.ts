9a3a4d6d28f042d5266844d231dcf02760c65753
sofia <sofiainesschenone@gmail.com>
2022-06-02 19:05:05 -0300

    m

--- src/app/services/servicefull.service.ts
+++ src/app/services/servicefull.service.ts
@@ -8,21 +8,21 @@ import { environment } from 'src/environments/environment.prod';
   providedIn: 'root'
 })
 export class ServicefullService {
-  api= environment.api;
+  apiPortafolio= environment.api;
   constructor(private http : HttpClient) {}
 
   //lista
   obtenerLista():Observable <Portfolio[]>{
-    return this.http.get<Portfolio[]>(`${this.api}/portafolio`);
+    return this.http.get<Portfolio[]>(`${this.apiPortafolio}/portafolio`);
   }
   editarPortfolio(id: number, portfolio: Portfolio):Observable <Object>{
-    return this.http.put(`${this.api}/portafolio/${id}`, portfolio);
+    return this.http.put(`${this.apiPortafolio}/portafolio/${id}`, portfolio);
   }
   eliminar(id: number):Observable <Object>{
-    return this.http.delete(`${this.api}/portafolio/${id}`);
+    return this.http.delete(`${this.apiPortafolio}/portafolio/${id}`);
   }
   //guardar
   guardarPortafolio(portafolio: Portfolio):Observable <Object>{
-    return this.http.post(`${this.api}/portafolio`, portafolio);
+    return this.http.post(`${this.apiPortafolio}/portafolio`, portafolio);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
-  /*firebase: {
-    projectId: 'portofiofullstack',
-    appId: '1:69953966698:web:cc57fe6f8ebbec1230b013',
-    storageBucket: 'portofiofullstack.appspot.com',
-    apiKey: 'AIzaSyCNPrVfL5SksEGfZ4RujYTCrnge6bGy4pU',
-    authDomain: 'portofiofullstack.firebaseapp.com',
-    messagingSenderId: '69953966698',
-  },*/
   production: true,
   firebaseConfig : {
     apiKey: "AIzaSyCNPrVfL5SksEGfZ4RujYTCrnge6bGy4pU",
@@ -16,5 +8,5 @@ export const environment = {
     messagingSenderId: "69953966698",
     appId: "1:69953966698:web:cc57fe6f8ebbec1230b013"
   },
-  api: "https://portafolio-sofia-schenone.herokuapp.com/api"
+  api: "https://portafolio-sofia-schenone.herokuapp.com/api",
 };
